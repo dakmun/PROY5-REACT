d640b5dcab9d676c9e54c3e76d437048879af8c4
Marcelo M. <mmundaca.27@outlook.cl>
2024-08-27 04:21:28 -0400

    chore: Update Vite configuration base path to '/PROY5-REACT/'

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PROY5/', // Reemplaza REPO_NAME con el nombre de tu repositorio
+  base: '/PROY5-REACT/', // Reemplaza REPO_NAME con el nombre de tu repositorio
   build: {
     rollupOptions: {
       output: {
