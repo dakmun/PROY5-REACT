cc0ea2e34d7099bcd76abbea2ea53bb40755b685
Marcelo M. <mmundaca.27@outlook.cl>
2024-08-27 02:48:22 -0400

    -

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/proy5/', // Reemplaza REPO_NAME con el nombre de tu repositorio
+  base: '/proy5/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: null,
+      },
+    },
+  },
 });
-
