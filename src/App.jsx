b91f3117d017ecf3a0ab4b4196dd6810cd999724
Marcelo M. <mmundaca.27@outlook.cl>
2024-08-27 05:15:37 -0400

    chore: Update router import in App.jsx and add 404.html page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/Homepage';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
