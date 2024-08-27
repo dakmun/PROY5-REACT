46c90f0165e47bce405187b78df8b333486d9cbc
Marcelo M. <mmundaca.27@outlook.cl>
2024-08-27 04:09:56 -0400

    chore: Update Vite configuration and package dependencies

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/PROY5/', // Reemplaza REPO_NAME con el nombre de tu repositorio
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: null,
+      },
+    },
+  },
+});
